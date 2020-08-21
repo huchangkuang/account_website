57707a0a08a60661e3c56fed697f28f54f3632c6
huchangkuang <603021137@qq.com>
2020-08-21 15:05:54 +0800

    deploy

---
+++ precache-manifest.094c9f92a0993685b8378a814071052f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce9a9a5ff60a2ee9456",
+    "url": "/account_website/css/app.ae5d7e40.css"
+  },
+  {
+    "revision": "95de5b62387c140c21fe2c9e4697d8e3",
+    "url": "/account_website/index.html"
+  },
+  {
+    "revision": "5ce9a9a5ff60a2ee9456",
+    "url": "/account_website/js/app.c3ca1df5.js"
+  },
+  {
+    "revision": "6746e7aee215db6fc907",
+    "url": "/account_website/js/chunk-vendors.4cdf2757.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/account_website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account_website/robots.txt"
+  }
+]);
